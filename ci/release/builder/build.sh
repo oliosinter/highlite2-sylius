7d9c6b9f56b34fe506feba0b6ad68ff3242b7d3b
oliosinter <olio.sinter@gmail.com>
2018-03-03 18:30:34 +0300

    building release with assets versioning

--- ci/release/builder/build.sh
+++ ci/release/builder/build.sh
@@ -4,6 +4,7 @@ composer install --no-dev --prefer-dist
 
 yarn install
 yarn run gulp
+yarn run rev
 
 tar --mtime='1970-01-01' -cf ../vendor.tar vendor/
 tar --mtime='1970-01-01' -cf ../assets.tar web/assets/
