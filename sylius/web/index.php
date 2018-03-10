63d42414997a0c1a1961e4267a1be5ad3ea393ec
oliosinter <olio.sinter@gmail.com>
2018-03-10 13:13:11 +0300

    current currency hack and error templates

--- sylius/web/index.php
+++ sylius/web/index.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * The default currency is EUR, since all highlite products have prices in EUR.
+ * But our channel needs products to have prices in RUB. Sylius doesn't handle
+ * this case properly: when default currency is EUR but the only available
+ * currency for channel is RUB - EUR is always displayed. This problem
+ * could be solved by setting cookie manually.
+ */
+$_COOKIE['_currency_default'] = 'RUB';
+
 $env = getenv('SYMFONY_ENV');
 
 if ($env == 'dev') {
